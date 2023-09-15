05949b58c7ca281f82b7b9486fa66c2417d93f72
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-09-15 04:20:43 -0500

    a tiny presentation layer

--- src/agivarsa.presentation/Renderer.vb
+++ src/agivarsa.presentation/Renderer.vb
@@ -1,14 +1,18 @@
 Public Module Renderer
+    Private Const GameTitle As String = "AGIVARSA"
+    Private Const GameSubtitle As String = "A Game in VB.NET About Requiring Some Assembly"
+    Private Const QuitText As String = "Quit"
+
     Public Sub Run()
-        Dim figlet As New FigletText("AGIVARSA") With
+        Dim figlet As New FigletText(GameTitle) With
             {
                 .Color = Color.Aqua,
                 .Justification = Justify.Center
             }
         AnsiConsole.Write(figlet)
-        AnsiConsole.WriteLine("A Game in VB.NET About Requiring Some Assembly")
+        AnsiConsole.WriteLine(GameSubtitle)
         Dim prompt As New SelectionPrompt(Of String) With {.Title = String.Empty}
-        prompt.AddChoice("Quit")
+        prompt.AddChoice(QuitText)
         AnsiConsole.Prompt(prompt)
     End Sub
 End Module
