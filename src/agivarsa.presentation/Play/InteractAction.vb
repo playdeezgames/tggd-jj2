bde5060db728f58ee65f9040af17a9c9dbd78b49
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-09-18 05:00:07 -0500

    i need characters to have unique names

--- src/agivarsa.presentation/Play/InteractAction.vb
+++ src/agivarsa.presentation/Play/InteractAction.vb
@@ -1,5 +1,8 @@
 ﻿Friend Module InteractAction
     Friend Function Run(model As IWorldModel) As Boolean
+        'interactable character names
+        'if only one, go straight to interacting
+        'otherwise, choose which character to interact with
         Throw New NotImplementedException()
     End Function
 End Module
