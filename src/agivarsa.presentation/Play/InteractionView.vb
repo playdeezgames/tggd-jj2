83227c53b970293273548acf11e9bca5eeb9f70f
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-09-19 04:05:17 -0500

    works.

--- src/agivarsa.presentation/Play/InteractionView.vb
+++ src/agivarsa.presentation/Play/InteractionView.vb
@@ -1,7 +1,14 @@
 ﻿Friend Module InteractionView
     Friend Function Run(model As IWorldModel) As Boolean
         Dim target = model.Avatar.InteractionTarget
-        MessageBox($"{model.Avatar.CharacterName} interacts with {target.name}.")
-        Return False
+        Dim prompt As New SelectionPrompt(Of String) With {.Title = $"[olive]Interact with {target.name}[/]"}
+        prompt.AddChoice(NeverMindText)
+        Dim answer = AnsiConsole.Prompt(prompt)
+        Select Case answer
+            Case NeverMindText
+                Return False
+            Case Else
+                Return False
+        End Select
     End Function
 End Module
